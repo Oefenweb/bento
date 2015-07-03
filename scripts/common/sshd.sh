fb7a61521d3f88310481ce319919d2b92e3ed5dd
Mischa ter Smitten <mischa@tersmitten.nl>
2015-07-03 09:35:32 +0200

    Removal of duplicate lines

--- scripts/common/sshd.sh
+++ scripts/common/sshd.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -eux
 
-sed -i '/UseDNS/d' /etc/ssh/sshd_config
-sed -i '/GSSAPIAuthentication/d' /etc/ssh/sshd_config
-
 echo "UseDNS no" >> /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
